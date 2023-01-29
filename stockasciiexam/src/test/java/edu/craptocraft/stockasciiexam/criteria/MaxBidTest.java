6fbe3813f2c7e96c2667e25c99dfdcd45cd064cd
ClearCB <abelcasasccb@gmail.com>
2023-01-29 12:43:49 +0100

    test(LastSale/MaxBid/MinAsk): add new test to check if criteria is implemented fine

---
+++ stockasciiexam/src/test/java/edu/craptocraft/stockasciiexam/criteria/MaxBidTest.java
@@ -0,0 +1,36 @@
+package edu.craptocraft.stockasciiexam.criteria;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import edu.craptocraft.stockasciiexam.item.Ask;
+import edu.craptocraft.stockasciiexam.item.Bid;
+import edu.craptocraft.stockasciiexam.item.Sale;
+import edu.craptocraft.stockasciiexam.item.Item;
+import edu.craptocraft.stockasciiexam.item.Sneaker;
+
+public class MaxBidTest {
+    
+    @Test
+    public void bidsCriteriaTest(){
+ 
+        MaxBid maxBid = new MaxBid();
+        Item sneaker = new Sneaker("555088-105", "Jordan 1 Retro High Dark Mocha");
+
+        sneaker.add(new Ask("13", 288));
+        sneaker.add(new Ask("6", 600));
+        sneaker.add(new Bid("6", 200));
+        sneaker.add(new Bid("9.5", 479));
+        sneaker.add(new Sale("6", 200));
+        sneaker.add(new Sale("9.5", 479));
+
+        
+        assertEquals(479,maxBid.checkCriteria(sneaker));
+        
+        sneaker.add(new Bid("6", 550));
+        sneaker.add(new Bid("9.5", 600));
+        assertEquals(600,maxBid.checkCriteria(sneaker));
+
+    }
+}
